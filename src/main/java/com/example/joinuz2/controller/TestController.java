2175bd550e42a4256535d6d0f250d9b15ff46fd9
jodonghyeon3 <jawoo1003@gmail.com>
2024-02-01 00:07:42 +0900

    프로젝트 셋팅

---
+++ src/main/java/com/example/joinuz2/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.joinuz2.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<?> test() {
+        return ResponseEntity.ok().build();
+    }
+
+}
